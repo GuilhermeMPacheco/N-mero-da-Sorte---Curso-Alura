11ebcfc31ddf9451a75119cc3866ea28e2d415b8
Guilherme Manoel Pacheco <guilhermempacheco2@gmail.com>
2024-09-23 21:17:24 -0300

    Aula 2 - Dia e Número

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
-alert("Boas vindas ao nosso site!");
+/* 
+alert("Boas Vindas ao meu Site!");
 
-let mensagemdeErro = "Erro! Preencha todos os campos"
-alert(mensagemdeErro);
+let numeroSecreto = 4;
+let chute = prompt('Escolha um número entre 1 e 10');
+console.log("O número Secreto é " + numeroSecreto);
+console.log(`Seu chute foi: ${chute}`);
+console.log(`Chute é igual ao número secreto? ${chute == numeroSecreto}`);
 
-let nome = prompt("Qual o seu nome?");
-let idade = prompt("Qual a sua idade?");
+// se chute for igual ao número secreto
+if(chute == numeroSecreto) {
+    alert(`Você acertou o número secreto! ${numeroSecreto}`)
+} else {
+    alert('Você errou :(')
+}
+    */
 
-if(idade >= 18) {alert("Pode tirar a habilitação!")}
-if(idade < 18) {alert("Não pode tirar a habilitação!")}
+// Desafio 1: Dia da Semana
+let DiaSemana = prompt("Qual é o dia da semana?");
+if(DiaSemana == "Sábado" || DiaSemana == "Domingo") {
+    alert("Bom fim de semana!")
+} else {
+    alert("Boa Semana!")
+}
+
+// Desafio 2: Número Negativo ou Positivo
+var numero = prompt("Escolha um número");
+if(numero < 0){alert('Seu número é negativo!')} else{
+if(numero == 0){alert('Seu número é 0!')} else{
+if(numero > 0){alert('Seu número é positivo!')} else{
+alert("Isso não é um número!")}}} 
